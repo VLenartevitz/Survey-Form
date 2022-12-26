ace3929f7d76b684f8309fdd1f13d033a2a5632e
Vinicius Lenartevitz <viniciuslenartevitz@gmail.com>
2022-12-26 12:47:02 -0300

    tirando a validação

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const radiobox = document.querySelectorAll('.radio')
 
-if (radiobox){
-    radiobox.forEach((radio)=>{
-        radio.addEventListener('change',()=>{
-            if(radio.checked){
-                const btn = document.querySelector('#btn');
-                btn.disabled = false 
-            }   
-        });
-         
-    })
-}
 
 
